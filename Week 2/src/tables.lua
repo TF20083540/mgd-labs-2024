897355064b440efe0462a39e60cb1517659944e6
20083540 <20083540@mail.wit.ie>
2024-09-16 22:19:13 +0100

    Exercise 4.1 complete

--- Week 2/src/tables.lua
+++ Week 2/src/tables.lua
@@ -1,3 +1,29 @@
 --Exercise 4.1 - Thomas Farrell, 20083540
 
-local fruitsByColour = {apples = "green",oranges = "orange", strawberries = "red"}
+local fruitsByColour = {apples = "green",
+    oranges = "orange",
+    strawberries = "red";
+}
+
+for key, value in pairs(fruitsByColour) do
+
+    --THIS DOES NOT WORK
+    --print(key)
+    --assert((i == 1 and key == "apples")
+    --    or (i == 2 and key == "oranges")
+    --    or (i == 3 and key == "strawberries"))
+
+
+    print(key .. " are typically " .. fruitsByColour[key])
+
+end
+
+print("\nAdding something else...\n");
+
+--This took WAY too long to figure out.
+fruitsByColour["lemons"] = "yellow"; --Appended a new key "lemons" and it's value "yellow" to the table.
+
+
+for index, value in pairs(fruitsByColour) do
+    print(index .. " are typically " .. fruitsByColour[index])
+end
